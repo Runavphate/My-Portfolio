bda6554931cbe7238ae4bf23860e246b91c2558f
Runav Phate <phaterunav@gmail.com>
2025-11-18 15:55:57 +0530

    Projects/voting-script.js    Projects/voting-style.css    Sources/Blockchain.png    Sources/FullStack.png    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function openImage(imageUrl) {
+  window.open(imageUrl, '_blank');
+}
+
+function openProject(projectUrl) {
+  window.open(projectUrl, '_blank');
+}
+
+function submitForm() {
+  alert("Response Recorded");
+}
